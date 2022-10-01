1378d8304e0c4111895fe491393d50a685940374
arpado <ozsvar.arpad@gmail.com>
2022-10-01 16:49:28 +0200

    trying to fix json import problems

--- pages/index.vue
+++ pages/index.vue
@@ -2,49 +2,63 @@
   <div class="main-container">
     <!-- <NuxtLayout name="default"> -->
       <ParallaxImageComponent
-        :imageName="this.content.images.imageMain"
+        imageName="bg-hero.jpg"
         :classConfig="'main-parallax'"
       >
         <TitleElement />
       </ParallaxImageComponent>
-      <!-- <SectionElement :content="sectionContent[0]"/> -->
+      <SectionElement :content="sectionContent[0]"/>
       <ParallaxImageComponent
-        :imageName="this.content.images.image1"
+        imageName="pizza2.jpg"
         :imgHeight="'400px'"
       >
       </ParallaxImageComponent>
-      <!-- <SectionElement :content="sectionContent[1]"/> -->
+      <SectionElement :content="sectionContent[1]"/>
       <ParallaxImageComponent
-        :imageName="this.content.images.image2"
+        imageName="pizza3.jpg"
         :imgHeight="'400px'"
       >
       </ParallaxImageComponent>
-      <!-- <ContactsElement :content="contacts"/> -->
+      <ContactsElement :content="contacts"/>
     <!-- </NuxtLayout> -->
   </div>
 </template>
 
-<script >
-import sectionContent from '~/content/hero-section-content.json'
-import contacts from '~/content/contacts.json'
+<script setup>
+    import sectionContent from '../content/hero-section-content.json'
+    import contacts from '../content/contacts.json'
 
+    // const content = {
+    //     images: {
+    //       imageMain: "bg-hero.jpg",
+    //       image1: "pizza2.jpg",
+    //       image2: "pizza3.jpg",
+    //     }
+    // }
+    // return { content }
+// export default {
+//   setup() {
 
-export default {
-  data() {
-    return {
-      content: {
-        images: {
-          imageMain: "bg-hero.jpg",
-          image1: "pizza2.jpg",
-          image2: "pizza3.jpg",
-        },
-      },
-    };
-  },
-  created() {
-    console.log(sectionContent[0]);
-  },
-};
+//     const sectionContent = require('~/content/hero-section-content.json')
+//     const contacts = require('~/content/contacts.json')
+//   },
+//   data() {
+//     return {
+//       content: {
+//         images: {
+//           imageMain: "bg-hero.jpg",
+//           image1: "pizza2.jpg",
+//           image2: "pizza3.jpg",
+//         },
+//         // sectionContent: this.sectionContent,
+//         // contacts: this.contacts,
+//       },
+//     };
+//   },
+//   created() {
+//     console.log(sectionContent[0]);
+//   },
+// };
 </script>
 
 <style>
