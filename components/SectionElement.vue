f01e91d7b99585a4b2fca634ee4288ba1c4c3550
arpado <ozsvar.arpad@gmail.com>
2022-11-07 23:08:09 +0100

    augmented-ui added, style modified

--- components/SectionElement.vue
+++ components/SectionElement.vue
@@ -4,7 +4,10 @@
     <p>
       {{ sectionContent.text }}
     </p> -->
-    <div class="card">
+    <div
+      data-augmented-ui="tl-scoop tr-scoop br-scoop bl-scoop both"
+      class="card"
+    >
       <div class="image card-subsection">
         <img :src="`/images/${content.image}`" alt="" />
       </div>
@@ -28,36 +31,45 @@ export default {
 <style scoped>
 .section {
   height: fit-content;
-  width: 1px;
-  min-height: 50vh;
-  min-width: 100%;
+  /* width: 1px; */
+  /* min-height: 50vh; */
+  width: 100%;
   padding: 2rem;
   background-color: var(--main-brown);
 }
 .card {
-  height: 100%;
+  height: fit-content;
   width: 100%;
   background-color: var(--main-white);
-  padding: 1rem;
+  padding: 2rem;
   display: flex;
+  justify-content: center;
+  align-items: center;
   flex-direction: column;
   gap: 1rem;
 }
 .card-subsection {
   width: 100%;
 }
+.image {
+  max-height: 500px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 img {
   width: 100%;
-  height: 100%;
+  /* height: 100%; */
   object-fit: cover;
 }
 h3 {
   text-align: center;
   padding: 1rem;
 }
-@media (min-width: 560px) {
+@media (min-width: 800px) {
   .section {
-  height: 1px;
+    /* height: 1px; */
   }
   .card {
     flex-direction: row;
