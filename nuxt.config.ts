c5da9350db1dc2ea4c8ca2b937c558e9a5749727
arpado <ozsvar.arpad@gmail.com>
2022-11-07 23:10:10 +0100

    pinia added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,28 @@
-import { defineNuxtConfig } from 'nuxt'
+// import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    // modules: [
-    //     '@nuxt/content'
-    // ],
+    modules: [
+        [
+            '@pinia/nuxt',
+            // {
+            //     autoImports: ['defineStore', 'acceptHMRUpdate'],
+            // },
+            {
+                autoImports: [
+                  // automatically imports `defineStore`
+                  'defineStore', // import { defineStore } from 'pinia'
+                  // automatically imports `defineStore` as `definePiniaStore`
+                  ['defineStore', 'definePiniaStore'], // import { defineStore as definePiniaStore } from 'pinia'
+                ],
+              },
+        ],
+        // '@nuxt/content',
+       
+    ],
+    imports: {
+        dirs: ['stores'],
+    },
     // content: {
         // https://content.nuxtjs.org/api/configuration
     // }
