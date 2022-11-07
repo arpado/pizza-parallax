8da6701be0a718f6bc6a1f27eb4a4d96fb0ab57b
arpado <ozsvar.arpad@gmail.com>
2022-11-07 23:06:12 +0100

    pinia added, card html refractored for styling, augmented ui added

--- components/MenuElement.vue
+++ components/MenuElement.vue
@@ -1,47 +1,88 @@
 <template>
-  <!-- <div class="section"> -->
-    <div class="card">
-      <!-- <div class="image card-subsection"> -->
-        <img :src="`images/${pizza.image}`" alt="" />
-      <!-- </div> -->
-      <!-- <div class="text-section card-subsection"> -->
+  <div class="card-background">
+    <div
+      data-augmented-ui="tl-scoop tr-scoop br-scoop bl-scoop both"
+      class="card"
+    >
+      <div class="pizza-title">
         <h3 class="pizza-name">{{ pizza.name }}</h3>
-        <div class="pizza-description">
-          <h4>Toppings:</h4>
-          <p>{{ pizza.description }}</p>
+      </div>
+      <div class="pizza-body">
+        <div class="image-section card-subsection">
+          <img :src="`images/${pizza.image}`" alt="" />
         </div>
-        <div class="pizza-price">
-          <p>Price:</p>
-          <p>${{ pizza.price }}</p>
+        <div class="text-section card-subsection">
+          <div class="pizza-description">
+            <h4>Toppings:</h4>
+            <p>{{ pizza.description }}</p>
+          </div>
+          <div class="pizza-price">
+            <p>Price:</p>
+            <p>${{ pizza.price }}</p>
+          </div>
         </div>
+      </div>
+      <div class="pizza-buttons">
         <div class="btn-bar">
           <div class="size-selector-bar">
-            <span class="size-selector btn-font" :class="{'size-selected': selectedPizzaSize === 'S'}" @click="changeSize('S')">S</span>
-            <span class="size-selector btn-font" :class="{'size-selected': selectedPizzaSize === 'M'}" @click="changeSize('M')">M</span>
-            <span class="size-selector btn-font" :class="{'size-selected': selectedPizzaSize === 'L'}" @click="changeSize('L')">L</span>
-            <span class="size-selector btn-font" :class="{'size-selected': selectedPizzaSize === 'XL'}" @click="changeSize('XL')">XL</span>
+            <span
+              class="size-selector btn-font"
+              :class="{ 'size-selected': selectedPizzaSize === 'S' }"
+              @click="changeSize('S')"
+              >S</span
+            >
+            <span
+              class="size-selector btn-font"
+              :class="{ 'size-selected': selectedPizzaSize === 'M' }"
+              @click="changeSize('M')"
+              >M</span
+            >
+            <span
+              class="size-selector btn-font"
+              :class="{ 'size-selected': selectedPizzaSize === 'L' }"
+              @click="changeSize('L')"
+              >L</span
+            >
+            <span
+              class="size-selector btn-font"
+              :class="{ 'size-selected': selectedPizzaSize === 'XL' }"
+              @click="changeSize('XL')"
+              >XL</span
+            >
           </div>
-          <div class="order-btn btn-font">Add to cart</div>
+          <div class="order-btn btn-font" @click="addToOrder(pizza.name)">
+            Add to cart
+          </div>
         </div>
-      <!-- </div> -->
-    <!-- </div> -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+// useCartStore.addToOrder(this.pizza.name)
+import { mapActions } from "pinia";
+import { useCartStore } from "../stores/cartStore";
+
 export default {
   name: "SectionElement",
   props: ["pizza"],
   data() {
     return {
-      selectedPizzaSize: 'L'
-    }
+      // pizzaName: this.pizza.name,
+      selectedPizzaSize: "L",
+    };
   },
   methods: {
     changeSize(value) {
-      this.selectedPizzaSize = value
-    }
-  }
+      this.selectedPizzaSize = value;
+    },
+    test(pizza) {
+      // console.log('poop')
+      console.log(pizza);
+    },
+    ...mapActions(useCartStore, ["addToOrder"]),
+  },
 };
 </script>
 
@@ -56,69 +97,83 @@ export default {
   padding: 15px;
   background-color: var(--main-brown);
 } */
+.card-background {
+  height: fit-content;
+  /* width: 100%; */
+  padding: 1rem;
+  background-color: var(--main-brown);
+}
 .card {
   /* height: 100%;
   width: 100%; */
-  height: 1px;
+  height: fit-content;
   min-height: 60vh;
   min-width: 380px;
   width: 80vw;
-  max-width: 600px;
-  border: 15px solid var(--main-brown);
+  max-width: 500px;
+  /* border: 15px solid var(--main-brown); */
 
   background-color: var(--main-white);
-  padding: 2%;
-  /* display: flex; */
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
   gap: 1rem;
-  display: grid;
+  /* display: grid;
   grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(12, 1fr);;
+  grid-template-rows: repeat(12, 1fr); */
 }
 /* .card-subsection {
   width: 50%;
 } */
+.image-section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 img {
   width: 100%;
-  height: 100%;
+  /* height: 100%; */
   object-fit: cover;
-  grid-column: 1/2;
-  grid-row: 1/13;
+  /* grid-column: 1/2;
+  grid-row: 1/13; */
 }
 .pizza-name {
   text-align: center;
   padding: 2rem;
-  grid-column: 2/3;
-  grid-row: 1/2;
+  /* grid-column: 2/3;
+  grid-row: 1/2; */
 }
 .pizza-description {
   text-align: center;
-  grid-column: 2/3;
-  grid-row: 2/4;
+  min-height: 8rem;
+  /* mi a line-height? */
+  /* grid-column: 2/3;
+  grid-row: 2/4; */
 }
+
 /* .text-section {
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
 } */
-
 .pizza-price {
   width: 100%;
   display: flex;
   justify-content: space-around;
   align-items: center;
-  grid-column: 2/3;
-  grid-row: 8/10;
+  /* grid-column: 2/3;
+  grid-row: 8/10; */
 }
 .btn-bar {
-    width: 100%;
+  width: 100%;
   display: flex;
   justify-content: space-around;
   align-items: center;
   flex-wrap: wrap;
   height: 200px;
-  grid-column: 2/3;
-  grid-row: 11/13;
+  /* grid-column: 2/3;
+  grid-row: 11/13; */
 }
 .size-selector {
   padding: 0.5rem 1rem;
@@ -140,6 +195,5 @@ img {
   background-color: var(--main-white);
   color: var(--main-black);
   border: 1px solid var(--main-black);
-  
 }
 </style>
