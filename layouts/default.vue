24101830267f5f1e03053da05fc5721dfb67678e
arpado <ozsvar.arpad@gmail.com>
2022-11-07 23:09:42 +0100

    cart menu added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <HeaderElement />
+    <!-- ezt innen lehet , h majd el -->
+    <CartElement />
     <slot />
     <FooterElement />
   </div>
 </template>
 
 <script>
+import { useCartStore } from '../stores/cartStore'
+
 export default {};
 </script>
 
