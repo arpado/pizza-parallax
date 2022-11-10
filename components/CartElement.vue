a3a2f55af472c3141bd89a618f12dd184274eeef
arpado <ozsvar.arpad@gmail.com>
2022-11-10 20:59:05 +0100

    send order button added, basic layout  added

--- components/CartElement.vue
+++ components/CartElement.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="cart-container" v-show="cartStore.showCart">
-      <h3>In Cart:</h3>
-      <ul>
-          <li v-for="(pizza, index) in cartStore.pizzaOnOrder" :key="index">
-              {{pizza.name}} - {{pizza.size}} - {{pizza.quantity}} - {{pizza.price}} - {{pizza.sumPrice.toFixed(2)}}
-            <button @click="cartStore.lessPizza(index)">--</button>
-            <button @click="cartStore.morePizza(index)">+</button>
-            <button @click="cartStore.deletePizza(pizza.name, pizza.size)">X</button>
-          </li>
-      </ul>
+    <div class="close-cart-arrow" @click="cartStore.toggleCart">close</div>
+    <h3 class="cart-title">In Cart:</h3>
+    <ul class="item-on-order-container">
+      <li v-for="(pizza, index) in cartStore.pizzaOnOrder" :key="index" class="item-on-order">
+        <div class="item-info">
+          {{ pizza.name }} - {{ pizza.size }} - {{ pizza.quantity }} -
+          {{ pizza.price }} - {{ pizza.sumPrice.toFixed(2) }}
+        </div>
+        <div class="item-buttons">
+          <button
+            class="order-manipulation-btn"
+            @click="cartStore.lessPizza(index)"
+          >
+            -
+          </button>
+          <button
+            class="order-manipulation-btn"
+            @click="cartStore.morePizza(index)"
+          >
+            +
+          </button>
+          <button
+            class="order-manipulation-btn"
+            @click="cartStore.deletePizza(pizza.name, pizza.size)"
+          >
+            X
+          </button>
+        </div>
+      </li>
+    </ul>
+    <div class="order-total">
       <p>total: {{ cartStore.totalPrice }}</p>
+    </div>
+    <div class="send-order">
+        <button>SEND ORDER</button>
+    </div>
   </div>
 </template>
 
@@ -17,22 +43,59 @@
 import { useCartStore } from "../stores/cartStore";
 
 export default {
-    setup() {
-        const cartStore = useCartStore()
-        return { cartStore }
-    }
-}
+  setup() {
+    const cartStore = useCartStore();
+    return { cartStore };
+  },
+};
 </script>
 
-<style>
+<style scoped>
 .cart-container {
-    width: 400px;
-    height: fit-content;
-    padding: 2rem;
-    background-color: white;
-    position: fixed;
-    top: 150px;
-    right: 0;
-    z-index: 999;
+  width: 600px;
+  height: 80vh;
+  padding: 2rem;
+  background-color: white;
+  position: fixed;
+  top: 18vh;
+  right: 10px;
+  z-index: 999;
+  display: grid;
+  grid-template-rows: repeat(12, 1fr);
+}
+.close-cart-arrow {
+    grid-row: 1/2;
+}
+.cart-title {
+    grid-row: 2/3;
+}
+.item-on-order-container {
+    grid-row: 3/10;
+    border: 1px solid black;
+    overflow: auto;
+    list-style: none;
+    font-size: initial;
+}
+.item-on-order{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.order-total {
+    grid-row: 10/11;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
+.send-order {
+    grid-row: 12/13;
+}
+
+
+.order-manipulation-btn {
+  width: 50px;
+  height: 50px;
+  margin: 10px;
+  background-color: var(--main-red);
 }
 </style>
