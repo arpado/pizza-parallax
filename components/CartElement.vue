ca532a718d35623102e4c2af92b208429e608f66
arpado <ozsvar.arpad@gmail.com>
2022-11-10 21:07:58 +0100

    formatting

--- components/CartElement.vue
+++ components/CartElement.vue
@@ -3,7 +3,11 @@
     <div class="close-cart-arrow" @click="cartStore.toggleCart">close</div>
     <h3 class="cart-title">In Cart:</h3>
     <ul class="item-on-order-container">
-      <li v-for="(pizza, index) in cartStore.pizzaOnOrder" :key="index" class="item-on-order">
+      <li
+        v-for="(pizza, index) in cartStore.pizzaOnOrder"
+        :key="index"
+        class="item-on-order"
+      >
         <div class="item-info">
           {{ pizza.name }} - {{ pizza.size }} - {{ pizza.quantity }} -
           {{ pizza.price }} - {{ pizza.sumPrice.toFixed(2) }}
@@ -34,7 +38,7 @@
       <p>total: {{ cartStore.totalPrice }}</p>
     </div>
     <div class="send-order">
-        <button>SEND ORDER</button>
+      <button>SEND ORDER</button>
     </div>
   </div>
 </template>
@@ -64,34 +68,32 @@ export default {
   grid-template-rows: repeat(12, 1fr);
 }
 .close-cart-arrow {
-    grid-row: 1/2;
+  grid-row: 1/2;
 }
 .cart-title {
-    grid-row: 2/3;
+  grid-row: 2/3;
 }
 .item-on-order-container {
-    grid-row: 3/10;
-    border: 1px solid black;
-    overflow: auto;
-    list-style: none;
-    font-size: initial;
+  grid-row: 3/10;
+  border: 1px solid black;
+  overflow: auto;
+  list-style: none;
+  font-size: initial;
 }
-.item-on-order{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+.item-on-order {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 .order-total {
-    grid-row: 10/11;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
+  grid-row: 10/11;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
 }
 .send-order {
-    grid-row: 12/13;
+  grid-row: 12/13;
 }
-
-
 .order-manipulation-btn {
   width: 50px;
   height: 50px;
