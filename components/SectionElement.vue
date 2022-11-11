819a067a2971bac274e291f553afa0ada2e86369
arpado <ozsvar.arpad@gmail.com>
2022-11-11 23:14:44 +0100

    layout added, styles changed

--- components/SectionElement.vue
+++ components/SectionElement.vue
@@ -1,24 +1,17 @@
 <template>
-  <div class="section">
-    <!-- <h3>{{ sectionContent.title }}</h3>
-    <p>
-      {{ sectionContent.text }}
-    </p> -->
-    <div
-      data-augmented-ui="tl-scoop tr-scoop br-scoop bl-scoop both"
-      class="card"
-    >
-      <div class="image card-subsection">
+  <NuxtLayout name="card">
+    <div class="container flex center-both gap-1">
+      <div class="subcontainer image flex center-both">
         <img :src="`/images/${content.image}`" alt="" />
       </div>
-      <div class="text card-subsection">
+      <div class="subcontainer text">
         <h3>{{ content.title }}</h3>
         <div class="text" v-for="(para, index) in content.text" :key="index">
           <p>{{ para }}</p>
         </div>
       </div>
     </div>
-  </div>
+  </NuxtLayout>
 </template>
 
 <script>
@@ -29,38 +22,18 @@ export default {
 </script>
 
 <style scoped>
-.section {
-  height: fit-content;
-  /* width: 1px; */
-  /* min-height: 50vh; */
-  width: 100%;
-  padding: 2rem;
-  background-color: var(--main-brown);
-}
-.card {
-  height: fit-content;
-  width: 100%;
-  background-color: var(--main-white);
-  padding: 2rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.container {
   flex-direction: column;
-  gap: 1rem;
 }
-.card-subsection {
+.subcontainer {
   width: 100%;
 }
 .image {
   max-height: 500px;
   overflow: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 }
 img {
   width: 100%;
-  /* height: 100%; */
   object-fit: cover;
 }
 h3 {
@@ -68,13 +41,10 @@ h3 {
   padding: 1rem;
 }
 @media (min-width: 800px) {
-  .section {
-    /* height: 1px; */
-  }
-  .card {
+  .container {
     flex-direction: row;
   }
-  .card-subsection {
+  .subcontainer {
     width: 50%;
   }
 }
