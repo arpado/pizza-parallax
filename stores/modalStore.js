c7a946f6872e37ec376f2526f56cc21e6e4bf7bf
arpado <ozsvar.arpad@gmail.com>
2022-11-16 13:37:22 +0100

    unnecessary parts deleted

--- stores/modalStore.js
+++ stores/modalStore.js
@@ -3,7 +3,6 @@ import { defineStore } from 'pinia';
 export const useModalStore = defineStore('modal', {
     state: () => {
         return {
-            // showModal: false,
             showLogin: false,
             showBooking: false,
             showItemSetup: false,
@@ -24,43 +23,19 @@ export const useModalStore = defineStore('modal', {
             this.showLogin = false
             this.showBooking = false
             this.showItemSetup = !this.showItemSetup
-            console.log(item)
         },
-        // openModal(content) {
-        //     this.showModal = true
-        //     this.modalContent = content
-        // },
         closeModal() {
-            // this.showModal = false
-            // this.modalContent = null
             this.showLogin = false
             this.showBooking = false
             this.showItemSetup = false
         },
         closeModalOnEscape(e) {
             if (e.keyCode == 27) {
-                // if (this.showMessage) {
-                //     this.closeMessageModal();
-                // } else if (this.showModal) {
-                //     this.closeModal();
-                // }
-
-                // if (this.showLogin) {
-                //     this.showLogin = false
-                // }
-                // if (this.showBooking) {
-                //     this.showBooking = false
-                // }
                 this.closeModal()
             }
         },
         closeOnOutsideClick(e) {
             if (e.target.classList.contains('modal-container')) {
-                // if (this.showMessage) {
-                //     this.closeMessageModal();
-                // } else if (this.showModal) {
-                //     this.closeModal();
-                // }
                 this.closeModal()
             }
         },
