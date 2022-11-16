293b2462961546cec60ebc748dbe57d3ab0cb93b
arpado <ozsvar.arpad@gmail.com>
2022-11-16 13:37:45 +0100

    init commit

---
+++ layouts/collapsible-container.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="collapsible-header flex justify-between">
+      <h3>{{ text }}</h3>
+      <button @click="toggleVisibility">Toggle</button>
+    </div>
+    <div class="collapsible-container" v-show="isVisible">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isVisible: false,
+    };
+  },
+  props: ["text"],
+  methods: {
+    toggleVisibility() {
+      this.isVisible = !this.isVisible;
+    },
+  },
+};
+</script>
+
+<style>
+.collapsible-header {
+  margin-top: 10px;
+}
+</style>
