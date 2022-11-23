e8d4749c586d3c6b1b1a33b1097287f2f8ab9140
arpado <ozsvar.arpad@gmail.com>
2022-11-23 17:29:05 +0100

    trying to fix payload errors in build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,7 @@ export default defineNuxtConfig({
     imports: {
         dirs: ['stores'],
     },
+    target: 'static',
     // content: {
         // https://content.nuxtjs.org/api/configuration
     // }
