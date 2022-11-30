75fca58040b041afefede27e86f3b9963cf8c7c4
arpado <ozsvar.arpad@gmail.com>
2022-11-30 10:46:45 +0100

    unnecessary elements commented out

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,19 +2,50 @@
   <div>
     <HeaderElement />
     <!-- ezt innen lehet , h majd el -->
-    <CartElement />
+    <!-- <CartElement /> -->
+    
+  <!-- <Transition name="content">
     <LoginElement />
-    <BookingElement />
+  </Transition> -->
+
+    <!-- <BookingElement /> -->
     <slot />
     <FooterElement />
   </div>
 </template>
 
 <script>
-import { useCartStore } from '../stores/cartStore'
+// import { useCartStore } from '../stores/cartStore'
 
 export default {};
 </script>
 
-<style>
+<style scoped>
+/* .slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+} */
+
+/* .content-enter-active,
+.content-leave-active {
+  transition: opacity 1s ease;
+  z-index: 999;
+}
+.content-enter-from,
+.content-leave-to {
+  opacity: 0;
+}
+.content-enter-to,
+.content-leave-from {
+  opacity: 1;
+} */
 </style>
