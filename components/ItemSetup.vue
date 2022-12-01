9b23815b385a44913779b94d86fa12abf76c8f54
arpado <ozsvar.arpad@gmail.com>
2022-12-01 18:17:04 +0100

    the dom rendering of the options panel redone, made it dynamic, also removed almost all of the logic to the itemModificationStore

--- components/ItemSetup.vue
+++ components/ItemSetup.vue
@@ -6,71 +6,58 @@
       <p v-if="itemModStore.itemOnMod">
         {{ itemModStore.itemOnMod.description }}
       </p>
-      <div class="pizza-buttons">
-        <NuxtLayout name="collapsible-container">
+      <div class="item-buttons">
+        <NuxtLayout
+          name="collapsible-container"
+          v-for="(prop, index) in itemModStore.itemOnMod.props"
+          :key="index"
+        >
           <template #title>
-            <h3>Select Size</h3>
+            <h3>{{ prop.description }}</h3>
           </template>
           <template #content>
             <div
-              class="size-selector"
-              v-for="size in sizeList"
-              :key="size.name"
+              class="radio-selector"
+              v-for="data in prop.data"
+              :key="data.name"
             >
               <input
                 type="radio"
-                name="size"
-                :value="size.name"
-                :id="size.name"
-                v-model="selectedPizzaSize"
+                :name="index"
+                :value="data.name"
+                :id="data.name"
+                v-model="itemModStore[prop.selector]"
               />
-              <label :for="size.name"
-                >{{ size.name }} -- ${{ size.price }}</label
+              <label :for="data.name"
+                >{{ data.name }} -- ${{ data.price }}</label
               >
             </div>
           </template>
         </NuxtLayout>
-        <NuxtLayout name="collapsible-container">
+        <NuxtLayout
+          name="collapsible-container"
+          v-for="(additionalOption, index) in itemModStore.additionalOptions"
+          :key="index"
+        >
           <template #title>
-            <h3>Select Crust</h3>
-          </template>
-          <template #content>
-            <div
-              class="crust-selector"
-              v-for="crust in crustList"
-              :key="crust.name"
-            >
-              <input
-                type="radio"
-                name="crust"
-                :value="crust.name"
-                :id="crust.name"
-                v-model="selectedCrust"
-              />
-              <label :for="crust.name"
-                >{{ crust.name }} -- +${{ crust.price }}</label
-              >
-            </div>
-          </template>
-        </NuxtLayout>
-        <NuxtLayout name="collapsible-container">
-          <template #title>
-            <h3>Select Additional Toppings</h3>
+            <h3>{{ additionalOption.title }}</h3>
           </template>
           <template #content>
             <div
               class="additional-topping"
-              v-for="top in toppingsList"
-              :key="top.name"
+              v-for="data in additionalOption.data"
+              :key="data.name"
             >
               <input
                 type="checkbox"
-                :name="top.name"
-                :id="top.name"
-                v-model="selectedToppings"
-                :value="top.name"
+                :name="data.name"
+                :id="data.name"
+                v-model="itemModStore.selectedOptions"
+                :value="data.name"
               />
-              <label :for="top.name">{{ top.name }} -- +${{ top.price }}</label>
+              <label :for="data.name"
+                >{{ data.name }} -- +${{ data.price }}</label
+              >
             </div>
           </template>
         </NuxtLayout>
@@ -85,21 +72,21 @@
       </div>
       <div class="price-container">
         <p>Total price:</p>
-        <p>{{ getSumPizzaPrice }}</p>
+        <p>{{ total }}</p>
       </div>
       <div class="flex justify-evenly">
-        <div class="pizza-quantity">
-          <button @click="fewerPizza">-</button>
+        <div class="item-quantity">
+          <button @click="itemModStore.fewerItem">-</button>
           <input
             type="number"
             name=""
             min="1"
-            id="pizza-quantity"
-            class="pizza-quantity-input"
-            v-model="selectedPizzaQuantity"
+            id="item-quantity"
+            class="item-quantity-input"
+            v-model="itemModStore.selectedItemQuantity"
           />
         </div>
-        <button @click="morePizza">+</button>
+        <button @click="itemModStore.moreItem">+</button>
         <button
           @click="
             sendToCart(
@@ -129,70 +116,22 @@ export default {
     const modalStore = useModalStore();
     const cartStore = useCartStore();
     const itemModStore = useItemModificationStore();
-    return { modalStore, cartStore, itemModStore };
+    const total = computed(() => itemModStore.getSumItemPrice);
+    return { modalStore, cartStore, itemModStore, total };
   },
   data() {
     return {
-      selectedPizzaQuantity: 1,
-      selectedPizzaSize: "medium",
-      selectedCrust: "thin",
-      selectedToppings: [],
-      sizeList: [
-        { name: "small", price: 0.99, isSelected: false },
-        { name: "medium", price: 1.49, isSelected: true },
-        { name: "large", price: 1.99, isSelected: false },
-        { name: "extra large", price: 2.49, isSelected: false },
-      ],
-      crustList: [
-        { name: "thin", price: 0.0, isSelected: true },
-        { name: "thick", price: 0.49, isSelected: false },
-        { name: "cheese", price: 0.99, isSelected: false },
-      ],
-      toppingsList: [
-        { name: "cheese", price: 0.99, isSelected: false },
-        { name: "ham", price: 1.49, isSelected: false },
-        { name: "mushroom", price: 1.49, isSelected: false },
-        { name: "salami", price: 1.99, isSelected: false },
-      ],
+      // itemProps: {
+      //   sizeList: this.getSize,
+      //   crustList: this.getCrust,
+      // },
     };
   },
-  methods: {
-    fewerPizza() {
-      if (this.selectedPizzaQuantity > 1) {
-        this.selectedPizzaQuantity--;
-      }
-    },
-    morePizza() {
-      this.selectedPizzaQuantity++;
-    },
-    sendToCart(name, size, quantity, crust, toppings, price) {
-      this.cartStore.addToOrder(name, size, quantity, crust, toppings, price);
-    },
-  },
-  computed: {
-    getSinglePizzaPrice() {
-      let selectedSize = this.sizeList.filter(
-        (elem) => elem.name === this.selectedPizzaSize
-      )[0].price;
-      let selectedCrust = this.crustList.filter(
-        (elem) => elem.name === this.selectedCrust
-      )[0].price;
-      let selectedTopPriceList = [];
-      this.selectedToppings.forEach((top) => {
-        let elem = this.toppingsList.filter((elem) => elem.name === top)[0]
-          .price;
-        selectedTopPriceList.push(elem);
-      });
-      let topPrice = selectedTopPriceList.reduce(
-        (accum, curr) => accum + curr,
-        0
-      );
-      return (selectedSize + selectedCrust + topPrice).toFixed(2);
-    },
-    getSumPizzaPrice() {
-      return (this.getSinglePizzaPrice * this.selectedPizzaQuantity).toFixed(2);
-    },
-  },
+  // methods: {
+  //   sendToCart(name, size, quantity, crust, toppings, price) {
+  //     this.cartStore.addToOrder(name, size, quantity, crust, toppings, price);
+  //   },
+  // },
 };
 </script>
 
@@ -207,13 +146,13 @@ export default {
 .container > * {
   margin-top: 30px;
 }
-.pizza-quantity-input {
+.item-quantity-input {
   width: 50px;
   text-align: center;
   -moz-appearance: textfield;
 }
-.pizza-quantity-input::-webkit-outer-spin-button,
-.pizza-quantity-input::-webkit-inner-spin-button {
+.item-quantity-input::-webkit-outer-spin-button,
+.item-quantity-input::-webkit-inner-spin-button {
   -webkit-appearance: none;
   margin: 0;
 }
