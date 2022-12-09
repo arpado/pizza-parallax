b029c13eab5546739b558795f5028a451c60b957
arpado <ozsvar.arpad@gmail.com>
2022-12-09 12:32:11 +0100

    init commit

---
+++ error.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper flex center-both column">
+    <NuxtLayout name="card">
+      <div class="flex center-both column">
+        <h3>Sorry, the page you're looking for isn't here...</h3>
+        <p>{{ error.statusCode }}</p>
+        <p>{{ error.message }}</p>
+        <p><NuxtLink to="/">Return to homepage</NuxtLink></p>
+      </div>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["error"],
+};
+</script>
+
+<style scoped>
+.wrapper {
+  width: 100vw;
+  height: 100vh;
+  background-image: url("/assets/pizza3.jpg");
+  background-size: cover;
+  background-position: center;
+}
+</style>
