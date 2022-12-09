550c8712ff2b4a9d90fd01373e3c878f53858a47
arpado <ozsvar.arpad@gmail.com>
2022-12-09 12:31:30 +0100

    tried to add anims

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,35 @@
 <template>
   <div class="main-container">
+    <!-- <Transition name="content"> -->
     <!-- <NuxtLayout name="default"> -->
-      <ParallaxImageComponent
-        imageName="bg-hero.jpg"
-        :classConfig="'main-parallax'"
-      >
-        <TitleElement />
-      </ParallaxImageComponent>
-      <SectionElement :content="sectionContent[0]"/>
-      <ParallaxImageComponent
-        imageName="pizza2.jpg"
-        :imgHeight="'400px'"
-      >
-      </ParallaxImageComponent>
-      <SectionElement :content="sectionContent[1]"/>
-      <ParallaxImageComponent
-        imageName="pizza3.jpg"
-        :imgHeight="'400px'"
-      >
-      </ParallaxImageComponent>
-      <ContactsElement :content="contacts"/>
+    <ParallaxImageComponent
+      imageName="bg-hero.jpg"
+      :classConfig="'main-parallax'"
+    >
+      <TitleElement />
+    </ParallaxImageComponent>
+    <SectionElement :content="sectionContent[0]" />
+    <ParallaxImageComponent imageName="pizza2.jpg" :imgHeight="'400px'"/>
+    <SectionElement :content="sectionContent[1]" />
+    <ParallaxImageComponent imageName="pizza3.jpg" :imgHeight="'400px'" />
+    <ContactsElement :content="contacts" />
     <!-- </NuxtLayout> -->
+    <!-- </Transition> -->
   </div>
 </template>
 
 <script setup>
-    import sectionContent from '../content/hero-section-content.json'
-    import contacts from '../content/contacts.json'
+import sectionContent from "../content/hero-section-content.json";
+import contacts from "../content/contacts.json";
 
-    // const content = {
-    //     images: {
-    //       imageMain: "bg-hero.jpg",
-    //       image1: "pizza2.jpg",
-    //       image2: "pizza3.jpg",
-    //     }
-    // }
-    // return { content }
+// const content = {
+//     images: {
+//       imageMain: "bg-hero.jpg",
+//       image1: "pizza2.jpg",
+//       image2: "pizza3.jpg",
+//     }
+// }
+// return { content }
 // export default {
 //   setup() {
 
@@ -62,9 +56,21 @@
 </script>
 
 <style>
-
 .main-container {
   height: 100%;
   width: 100%;
+}
+.content-enter-active,
+.content-leave-active {
+  transition: opacity 1s ease;
+  z-index: 999;
+}
+.content-enter-from,
+.content-leave-to {
+  opacity: 0;
+}
+.content-enter-to,
+.content-leave-from {
+  opacity: 1;
 }
 </style>
