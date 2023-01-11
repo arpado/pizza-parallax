0476ce9183c230e04410d2d15314165b4e3898ce
arpado <ozsvar.arpad@gmail.com>
2023-01-11 20:52:16 +0100

    modifyItem function changed, styles changed, cleanup

--- components/MenuElement.vue
+++ components/MenuElement.vue
@@ -1,32 +1,32 @@
 <template>
-<div class="additional-dimensions">
-   <NuxtLayout name="card" >
-    <div class="item-title">
-      <h3 class="item-name">{{ item.name }}</h3>
-    </div>
-    <div class="item-body">
-      <div class="image-section card-subsection">
-        <img :src="`images/${item.image}`" alt="" />
+  <div class="additional-dimensions">
+    <NuxtLayout name="card">
+      <div class="item-title">
+        <h3 class="item-name">{{ item.name }}</h3>
       </div>
-      <div class="text-section card-subsection">
-        <div class="item-description">
-          <h4>Toppings:</h4>
-          <p>{{ item.description }}</p>
+      <div class="item-body">
+        <div class="flex center-both">
+          <img :src="item.image" alt="" />
         </div>
-        <div class="item-price">
-          <p>Price starts from:</p>
-          <p v-if="item.props">${{ item.props.sizeList.data[0].price }}</p>
+        <div>
+          <div class="item-description">
+            <h4>Toppings:</h4>
+            <p>{{ item.description }}</p>
+          </div>
+          <div class="item-price flex align-center justify-between">
+            <p>Price starts from:</p>
+            <p>${{ item.price }}</p>
+          </div>
         </div>
       </div>
-    </div>
-    <button
-      class="order-btn btn-font flex center-both"
-      @click="modifyItem(item)"
-    >
-      Select Item
-    </button>
-  </NuxtLayout>
-  </div> 
+      <button
+        class="order-btn btn-font flex center-both"
+        @click="modifyItem(item, activeTable)"
+      >
+        Select Item
+      </button>
+    </NuxtLayout>
+  </div>
 </template>
 
 <script>
@@ -35,16 +35,23 @@ import { useItemModificationStore } from "~/stores/itemModificationStore";
 
 export default {
   name: "SectionElement",
-  props: ["item"],
+  props: ["item", "activeTable"],
   setup() {
     const modalStore = useModalStore();
     const itemModStore = useItemModificationStore();
     return { modalStore, itemModStore };
   },
   methods: {
-    modifyItem(item) {
-      this.itemModStore.loadItem(item);
-      this.modalStore.openModal('showItemSetup')
+    async modifyItem(item, table) {
+      let itemToBeModified = {
+        name: item.name,
+        id: item.id,
+        table: table,
+        image: item.image,
+        description: item.description,
+      };
+      this.itemModStore.loadItem(itemToBeModified, table);
+      this.modalStore.openModal("showItemSetup");
     },
   },
 };
@@ -55,12 +62,7 @@ export default {
   min-height: 60vh;
   min-width: 380px;
   width: 80vw;
-  max-width: 500px;
-}
-.image-section {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  max-width: 350px;
 }
 img {
   width: 100%;
@@ -68,47 +70,26 @@ img {
 }
 .item-name {
   text-align: center;
-  padding: 2rem;
+  margin-bottom: 1rem;
 }
 .item-description {
+  min-height: 6rem;
+  margin: 1rem 0;
+}
+.item-description > h4 {
   text-align: center;
-  min-height: 8rem;
+  margin-bottom: 0.5rem;
 }
 .item-price {
   width: 100%;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-}
-.item-buttons {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-}
-.btn-bar {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  flex-wrap: wrap;
-}
-.item-quantity {
-}
-.item-quantity-input {
-  width: 100px;
+  margin: 1rem 0;
 }
 .order-btn {
   padding: 1rem 2rem;
+  margin: 1rem auto 0;
   background-color: var(--main-red);
   color: var(--main-black);
   border: 1px solid var(--main-black);
-  margin: 0 auto;
-}
-.order-btn {
   cursor: pointer;
-}
-.size-selected {
-  background-color: var(--main-white);
-  color: var(--main-black);
-  border: 1px solid var(--main-black);
 }
 </style>
