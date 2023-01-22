9224e0f5c796e9fe36bea944df299ccf498ea51b
arpado <ozsvar.arpad@gmail.com>
2023-01-22 18:38:30 +0100

    user data modification setup added, user order history section added

--- pages/user-profile.vue
+++ pages/user-profile.vue
@@ -1,13 +1,36 @@
 <template>
   <div class="wrapper flex column center-both">
-    <h3>user data:</h3>
-    <div class="user-container">
-      {{ userStore.user }}
+    <h3>User Profile:</h3>
+    <div class="data-container flex column" v-if="userStore.user">
+      <div class="data-unit" v-for="(value, key) in userStore.user" :key="key">
+        <div
+          :class="key"
+          class="flex justify-between"
+          v-if="activeSetup === key"
+        >
+          <div>
+            <input class="input-field" type="text" v-model="userStore.user[key]" />
+            <button @click="toggleSetup(key)">Cancel</button>
+            <button @click="userStore.updateUser(key, userStore.user[key], userStore.user.id)">Save</button>
+          </div>
+          <!-- <a @click="toggleSetup(key)">Modify data</a> -->
+        </div>
+        <div class="flex justify-between" v-else>
+          <span>{{ key }}</span> -- <span>{{ value }}</span> --
+          <a @click="toggleSetup(key)">Modify data</a>
+        </div>
+      </div>
+      <h3>User History</h3>
+      <div class="flex column" v-if="userStore.orderHistory">
+        <div class="data-unit-RENAME" v-for="(item, index) in userStore.orderHistory" :key="index">
+          <p>{{ item }}</p>
+        </div>
+        <button @click="userStore.getUserOrderHistory">Get History</button>
+      </div>
     </div>
-    <div class="user-container">
-      {{ userFromDatabase }}
+    <div v-else>
+      <h3>No user logged in!</h3>
     </div>
-    <button @click="getData">get user</button>
   </div>
 </template>
 
@@ -21,28 +44,54 @@ export default {
   },
   data() {
     return {
-      userFromDatabase: null,
+      // userData: {
+      //   "First Name": this.userStore.user.firstName,
+      //   "Last Name": this.userStore.user.lastName,
+      //   Email: this.userStore.user.email,
+      //   Phone: this.userStore.user.phone,
+      //   Address: this.userStore.user.address,
+      // },
+      activeSetup: null,
     };
   },
   methods: {
-    async getData() {
-      // ezt userstorebol szedni, es csak a modositas eseten visszakuldeni a dolgot
-      if (this.userStore.user) {
-        this.userFromDatabase = await getUserFromDB();
-      } else {
-        this.userFromDatabase = null;
+    toggleSetup(key) {
+      if (this.activeSetup === key) {
+        this.activeSetup = null;
+        return;
       }
+      this.activeSetup = key;
     },
+ 
+    // async getData() {
+    //   // ezt userstorebol szedni, es csak a modositas eseten visszakuldeni a dolgot
+    //   if (this.userStore.user) {
+    //     this.userFromDatabase = await getUserFromDB();
+    //   } else {
+    //     this.userFromDatabase = null;
+    //   }
+    // },
   },
+  // mounted() {
+  //   this.getData()
+  // }
 };
 </script>
 
 <style scoped>
 .wrapper {
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
+  color: var(--main-white);
 }
-.user-container {
+.data-container {
   border: 1px solid black;
+  width: 360px;
+}
+.data-unit {
+  height: 100px;
+}
+.input-field {
+  width: 100 px;
 }
 </style>
