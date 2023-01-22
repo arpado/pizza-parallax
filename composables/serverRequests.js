d5c95df25aa8413e62b5a9135d780f7fcd3628cd
arpado <ozsvar.arpad@gmail.com>
2023-01-22 18:34:29 +0100

    sendRequest method modded to send additional data to the database (to note elements that belong together), getOrderHistory method added

--- composables/serverRequests.js
+++ composables/serverRequests.js
@@ -25,27 +25,31 @@ async function sendOrder(orderedArray) {
   const userJSON = localStorage.getItem('sb-qykublxyqkhmvdpnkezp-auth-token')
   if (userJSON) {
     user = JSON.parse(userJSON)
-    console.log(user.user.id)
   } else {
     console.log('No user found!')
   }
-  // collect orders
+  // collect orders (az index nem tudom mert maradt benne)
   orderedArray.forEach(async (elem, index) => {
     if (elem.selectedOptions.length) {
       elem.selectedOptions.forEach(async option => {
         const { error } = await supabase
-          .from('order')
-          .insert({ user_id: user.user.id, item_id: elem.id, item_quantity: elem.quantity, item_option: option });
+          .from('orders')
+          .insert({ order_item_id: elem.orderUnitId, user_id: user.user.id, item_id: elem.id, item_quantity: elem.quantity, item_option: option });
         if (error) {
           console.log(error);
         }
       })
     } else {
       const { error } = await supabase
-        .from('order')
-        .insert({ user_id: user.user.id, item_id: elem.id, item_quantity: elem.quantity, item_option: null });
+        .from('orders')
+        .insert({ order_item_id: elem.orderUnitId, user_id: user.user.id, item_id: elem.id, item_quantity: elem.quantity, item_option: null });
     }
   })
 }
 
-export { getItemData, sendOrder }
+
+async function getOrderHistory(userId) {
+  return await supabase.from('orders').select().eq('user_id', userId)
+}
+
+export { getItemData, sendOrder, getOrderHistory }
