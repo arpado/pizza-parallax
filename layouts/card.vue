9d52edff11f7a84807d85e02e2bd057c5190f498
arpado <ozsvar.arpad@gmail.com>
2023-01-31 21:25:47 +0100

    cleanup

--- layouts/card.vue
+++ layouts/card.vue
@@ -16,31 +16,13 @@ export default {};
 <style scoped>
 .card-background {
   height: fit-content;
-  /* width: 100%; */
   padding: 1rem;
   background-color: var(--main-brown);
   position: relative;
 }
 .card-foreground {
-  /* height: 100%;
-  width: 100%; */
   height: fit-content;
-
-    /* height: fit-content; */
-  /* width: 1px; */
-  /* min-height: 50vh; */
-  /* width: 100%; */
-  /* padding: 1rem;
-  background-color: var(--main-brown); */
-  /* border: 15px solid var(--main-brown); */
-
   background-color: var(--main-white);
   padding: 2rem;
-  /* display: flex;
-  flex-direction: column; 
-  gap: 1rem;*/
-  /* display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(12, 1fr); */
 }
 </style>
