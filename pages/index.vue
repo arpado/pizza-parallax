ad9691537b9d7dea5302c4bc0d39cf3b57596f0d
arpado <ozsvar.arpad@gmail.com>
2023-01-31 21:36:23 +0100

    scoped tag added to style tag, cleanup

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="main-container">
-    <!-- <Transition name="content"> -->
-    <!-- <NuxtLayout name="default"> -->
     <ParallaxImageComponent
       imageName="bg-hero.jpg"
       :classConfig="'main-parallax'"
@@ -9,53 +7,19 @@
       <TitleElement />
     </ParallaxImageComponent>
     <SectionElement :content="sectionContent[0]" />
-    <ParallaxImageComponent imageName="pizza2.jpg" :imgHeight="'400px'"/>
+    <ParallaxImageComponent imageName="pizza2.jpg" :imgHeight="'400px'" />
     <SectionElement :content="sectionContent[1]" />
     <ParallaxImageComponent imageName="pizza3.jpg" :imgHeight="'400px'" />
     <ContactsElement :content="contacts" />
-    <!-- </NuxtLayout> -->
-    <!-- </Transition> -->
   </div>
 </template>
 
 <script setup>
 import sectionContent from "../content/hero-section-content.json";
 import contacts from "../content/contacts.json";
-
-// const content = {
-//     images: {
-//       imageMain: "bg-hero.jpg",
-//       image1: "pizza2.jpg",
-//       image2: "pizza3.jpg",
-//     }
-// }
-// return { content }
-// export default {
-//   setup() {
-
-//     const sectionContent = require('~/content/hero-section-content.json')
-//     const contacts = require('~/content/contacts.json')
-//   },
-//   data() {
-//     return {
-//       content: {
-//         images: {
-//           imageMain: "bg-hero.jpg",
-//           image1: "pizza2.jpg",
-//           image2: "pizza3.jpg",
-//         },
-//         // sectionContent: this.sectionContent,
-//         // contacts: this.contacts,
-//       },
-//     };
-//   },
-//   created() {
-//     console.log(sectionContent[0]);
-//   },
-// };
 </script>
 
-<style>
+<style scoped>
 .main-container {
   height: 100%;
   width: 100%;
