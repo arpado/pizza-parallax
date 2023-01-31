56cc6f14dbdc522089b7b58d48019ab7d662d8c2
arpado <ozsvar.arpad@gmail.com>
2023-01-31 21:49:48 +0100

    mapState imported to access userID from userStore, sendOrderData() is called from here now on to make orders, addToOrder() modified to log errors, lockOrder() added and sendOrder() redone and moved in here from serverRequests.js, comments added

--- stores/cartStore.js
+++ stores/cartStore.js
@@ -1,11 +1,16 @@
 import { defineStore } from 'pinia'
 import { v4 as uuidv4 } from 'uuid';
+import { mapState } from 'pinia'
+import { useUserStore } from './userStore'
+import { sendOrderData } from '@/composables/serverRequests'
 
 export const useCartStore = defineStore('cart', {
   state: () => {
     return {
-      showCart: false,
       itemOnOrder: [],
+      lockedOrder: [],
+      lockedOrderID: null,
+      sentOrderID: null,
     }
   },
   getters: {
@@ -17,17 +22,19 @@ export const useCartStore = defineStore('cart', {
       return totalArr.reduce(
         (total, sumPrice) => total + sumPrice,
         0).toFixed(2)
-    }
+    },
+    ...mapState(useUserStore, ['userId']),
   },
   actions: {
+    // checks if array A equlas array B
     checkSelectedOptions(a, b) {
       return Array.isArray(a) &&
         Array.isArray(b) &&
         a.length === b.length &&
         a.every((val, index) => val === b[index]);
     },
+    // if the item with its exact optional settings is already on the list adds one the the counter, otherwise adds the item to the list 
     addToOrder(item) {
-      // ha a keyek egyeznek, akkor adja elozohoz, amugy meg ujat csinal
       let itemFound = false
       this.itemOnOrder.forEach(elem => {
         // ide komplexebb validatort
@@ -40,11 +47,11 @@ export const useCartStore = defineStore('cart', {
       })
 
       if (!itemFound && item.quantity > 0) {
-        // ez atteve itemOnModra (???)
         item.orderUnitId = uuidv4();
         this.itemOnOrder.push(item)
-      }
-      console.log(this.itemOnOrder)
+      } else {
+        console.log('Error in addToOrder()')
+      } 
     },
     moreItem(index) {
       this.itemOnOrder[index].quantity++
@@ -58,6 +65,32 @@ export const useCartStore = defineStore('cart', {
     },
     deleteItem(name, size) {
       this.itemOnOrder = this.itemOnOrder.filter(elem => { return (elem.name !== name || elem.size !== size) })
-    }
+    },
+    lockOrder() {
+      this.lockedOrder = this.itemOnOrder
+      this.lockedOrderID = uuidv4();
+    },
+    // calls the API function from the serverRequests.js
+    // !!! a uuid should be given as a ID for each order, maybe on the end of addToOrder, then should be sent back here from the server, and put in a var, and get this checked for === before sending to make sure no multi-sending the same order is possible
+    async sendOrder() {
+      if (!this.userId) {
+        console.log('No user found!')
+        return
+      }
+      if (this.lockedOrderID === this.sentOrderID) {
+        console.log('This order has already been  sent!')
+        return
+      }
+      
+      // if (this.userId && ) {
+        let error = await sendOrderData(this.lockedOrder, this.userId)
+        if (error) {
+          console.log(error)
+        } else {
+          this.sentOrderID = this.lockedOrderID
+        }
+      // } else {
+      // }
+    },
   },
 })
