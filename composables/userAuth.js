c3cf53f48f6a20f176d659fda20259704b732f6d
arpado <ozsvar.arpad@gmail.com>
2023-01-31 21:24:24 +0100

    comments added

--- composables/userAuth.js
+++ composables/userAuth.js
@@ -1,5 +1,6 @@
 import supabase from "./supabaseClient";
 
+// built-in supabase register function
 async function registerWithEmail(email, password, firstName, lastName, address) {
     return await supabase.auth.signUp({
         email: email,
@@ -14,6 +15,7 @@ async function registerWithEmail(email, password, firstName, lastName, address) 
     });
 }
 
+// built-in supabase login function
 async function postLoginForm(email, password) {
     return await supabase.auth.signInWithPassword({
         email: email,
@@ -21,14 +23,17 @@ async function postLoginForm(email, password) {
     });
 }
 
+// built-in supabase logout function
 async function postSignOut() {
     return await supabase.auth.signOut();
 }
 
+// gets the user data from the public schema, but cannot access data from the users table in the auth schema due to supabase restrictions
 async function getUserData(userID) {
     return await supabase.from("profiles").select('id, first_name, last_name, address').eq("id", userID);
 }
 
+// updates user data in the public schema, helperObj has to be used to make the column selectable
 async function updateUserData(table, column, data, userId) {
     let helperObj = {}
     helperObj[column] = data
@@ -38,13 +43,14 @@ async function updateUserData(table, column, data, userId) {
         .eq('id', userId)
 }
 
+// updates user data in the auth schema, helperObj has to be used to make the column selectable
 async function updateProfileData(column, data) {
     let helperObj = {}
     helperObj[column] = data
     return await supabase.auth.updateUser(helperObj)
 }
 
-// this has to be done on the server
+// -- to be modified later -- delete profile, but this has to be done on the server later on for security reasons
 async function deleteProfileData(userId) {
     const { error } = await supabase
         .from('profiles')
