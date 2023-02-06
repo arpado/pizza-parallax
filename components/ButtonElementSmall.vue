569e4888b075296935da9c3cce46a355909ce2ce
arpado <ozsvar.arpad@gmail.com>
2023-02-06 12:03:09 +0100

    init commit

---
+++ components/ButtonElementSmall.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="button flex center-both" tabindex="0">
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+// @click="doThis"
+export default {
+  props: ["text"],
+  // methods: {
+  //   doThis(func, arg) {
+
+  //   }
+  // }
+};
+</script>
+
+<style scoped>
+.button {
+  height: 50px;
+  width: 50px;
+  min-width: fit-content;
+  margin: 10px;
+  padding: 1rem;
+  border: 1px solid var(--main-white);
+  border-radius: 10px;
+  background-color: var(--main-red);
+  font-weight: bold;
+  font-size: 1.3rem;
+  transition: background-color 0.5s, color 0.5s, box-shadow 0.025s,
+    border 0.025s;
+}
+.button:hover {
+  cursor: pointer;
+  background-color: var(--main-white);
+  color: var(--main-black);
+}
+.button:active {
+  box-shadow: 0 0 10px 1px var(--main-white);
+  border: 5px inset black;
+}
+</style>
---
+++ components/MainImageElement.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <!-- <img src="@/public/images/pizza-tray.png" alt=""> -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  /* width: 795px; */
+  /* height: 595px; */
+  height: 80vh;
+  aspect-ratio: 4/3;
+  margin-top: 100px;
+  border-radius: 40px;
+  box-shadow: 10px 10px 25px 2px;
+  right: calc(75% - 280px);
+  top: calc(50% - 50vh);
+  position: absolute;
+  /* bg image */
+  background-size: auto 102%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-image: url("/images/pizza-tray.png");
+}
+</style>
