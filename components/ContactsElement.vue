b8481aa4d22cf95b5afc1d0491b612a858ae9b6c
arpado <ozsvar.arpad@gmail.com>
2023-02-06 11:50:16 +0100

    style modified, markup modified

--- components/ContactsElement.vue
+++ components/ContactsElement.vue
@@ -6,20 +6,26 @@
       </div>
       <div class="text card-subsection">
         <h3>{{ content.title }}</h3>
-        <div class="flex justify-around">
+        <div class="contents-container flex justify-around">
           <div class="text">
             <h4 class="contacts-subtitle flex center-both">Address:</h4>
-            <p v-for="(para, index) in content.text" :key="index">{{ para }}</p>
+            <ul>
+              <li v-for="(para, index) in content.text" :key="index">
+                {{ para }}
+              </li>
+            </ul>
           </div>
           <div class="text">
             <h4 class="contacts-subtitle flex center-both">Open Hours:</h4>
-            <p>Monday: {{ content.hours.closed }}</p>
-            <p>Tuesday: {{ content.hours.regular }}</p>
-            <p>Wednesday: {{ content.hours.regular }}</p>
-            <p>Thursday: {{ content.hours.regular }}</p>
-            <p>Friday: {{ content.hours.extended }}</p>
-            <p>Staruday: {{ content.hours.extended }}</p>
-            <p>Sunday: {{ content.hours.extended }}</p>
+            <ul>
+              <li>Monday: {{ content.hours.closed }}</li>
+              <li>Tuesday: {{ content.hours.regular }}</li>
+              <li>Wednesday: {{ content.hours.regular }}</li>
+              <li>Thursday: {{ content.hours.regular }}</li>
+              <li>Friday: {{ content.hours.extended }}</li>
+              <li>Staruday: {{ content.hours.extended }}</li>
+              <li>Sunday: {{ content.hours.extended }}</li>
+            </ul>
           </div>
         </div>
       </div>
@@ -47,6 +53,9 @@ img {
   max-height: 400px;
   object-fit: cover;
 }
+.contents-container {
+  flex-direction: column;
+}
 h3 {
   text-align: center;
   padding: 1rem;
@@ -54,12 +63,21 @@ h3 {
 .contacts-subtitle {
   margin: 10px;
 }
+ul {
+  list-style: none;
+  text-align: center;
+}
 @media (min-width: 560px) {
   .container {
     flex-direction: row;
   }
   .card-subsection {
     width: 50%;
+  }
+}
+@media (min-width: 800px) {
+  .contents-container {
+    flex-direction: row;
   }
 }
 </style>
