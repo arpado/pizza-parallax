272a4baff930f7a82031e7616087ff714245e90c
arpado <ozsvar.arpad@gmail.com>
2023-02-06 11:47:53 +0100

    style modified, span tag added

--- components/ButtonElement.vue
+++ components/ButtonElement.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="button" tabindex="0">
-    <p>{{ text }}</p>
+  <div class="button flex center-both" tabindex="0">
+    <span>{{ text }}</span>
   </div>
 </template>
 
@@ -16,14 +16,19 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .button {
-  height: 100px;
-  width: 200px;
+  min-height: 50px;
+  height: fit-content;
+  /* min-width: 200px; */
+  width: fit-content;
+  max-width: 100%;
+  padding: 1rem;
   border: 1px solid var(--main-white);
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  border-radius: 10px;
+  background-color: var(--main-red);
+  font-weight: bold;
+  font-size: 1.3rem;
   transition: background-color 0.5s, color 0.5s, box-shadow 0.025s,
     border 0.025s;
 }
@@ -34,6 +39,9 @@ export default {
 }
 .button:active {
   box-shadow: 0 0 10px 1px var(--main-white);
-  border: 5px inset black;
+  border: 1px inset black;
+}
+span {
+  text-align: center;
 }
 </style>
