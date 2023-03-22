dc6232e474cf4de4986d4b68229af5401d04347d
arpado <ozsvar.arpad@gmail.com>
2023-03-22 22:10:35 +0100

    code rewritten

--- tests/unit/components/BookingElement.test.js
+++ tests/unit/components/BookingElement.test.js
@@ -1,60 +1,87 @@
-// import { mount, shallowMount } from '@vue/test-utils'
-// import BookingElement from '@/components/BookingElement.vue'  // Import Vue component to test
-// import { createPinia, setActivePinia, defineStore } from 'pinia'
-// import { createTestingPinia } from '@pinia/testing'
-// import { useModalStore } from '@/stores/modalStore'
+import { mount, shallowMount } from '@vue/test-utils'
+import BookingElement from '@/components/BookingElement.vue'  // Import Vue component to test
+import { createPinia, setActivePinia, defineStore } from 'pinia'
+import { createTestingPinia } from '@pinia/testing'
+import { useModalStore } from '@/stores/modalStore'
 
-// describe('Checking showBooking functionality', () => {
+describe('Checking showBooking functionality', async () => {
 
-// let wrapper = null;
+  let wrapper = null;
 
-//   beforeEach(() => {
-//     // render the component
-//     wrapper = mount(BookingElement, {
-//       global: {
-//         plugins: [createTestingPinia()],
-//       },
-//     })    
-    
-//     // set any initial data and create the mocks of libraries
-//   })
-
-//   afterEach(() => {
-//     wrapper.unmount()
-//   })
-
-//   test('{ showBooking: true } should display element', () => {
-//     const modalStore = useModalStore()
-//     modalStore.modalList.showBooking = true
-//     expect(wrapper.findAll('h3').at(0).text()).toMatch('Book Your Table')
-//   });
-
-//   test('{ showBooking: false } should not display element', () => {
-//     const modalStore = useModalStore()
-//     modalStore.modalList.showBooking = false
-//     expect(wrapper).toBeNull()
-//   });
-
-// //   it('check successful events', () => { ... })
-
-// //   it('check failure events', () => { ... })
-// })
-
-import { mount } from '@vue/test-utils'
-
-// The component to test
-const MessageComponent = {
-  template: '<p>{{ msg }}</p>',
-  props: ['msg']
-}
-
-test('displays message', () => {
-  const wrapper = mount(MessageComponent, {
-    props: {
-      msg: 'Hello world'
-    }
+  afterEach(() => {
+    wrapper.unmount()
   })
 
-  // Assert the rendered text of the component
-  expect(wrapper.text()).toContain('Hello world')
+  test('{ showBooking: true } should display element', () => {
+    wrapper = mount(BookingElement, {
+      global: {
+        plugins: [createTestingPinia({
+          // createSpy: vi.fn,
+          initialState: {
+            modal: {
+              modalList: {
+                showBooking: true,
+              }
+            }
+          }
+        })],
+      },
+    })
+    // const wrapper = mount(Counter, {
+    //   global: {
+    //     plugins: [createTestingPinia()],
+    //   },
+    // })
+    
+    // const store = useSomeStore() // uses the testing pinia!
+
+    // const store = useModalStore()
+    // store.$patch({
+    //   modalList: {
+    //     showBooking: true,
+    //   }
+    // })
+
+    expect(wrapper.isVisible()).toBe(true)
+  });
+
+  test('{ showBooking: false } should not display element', () => {
+    wrapper = mount(BookingElement, {
+      global: {
+        plugins: [createTestingPinia({
+          // createSpy: vi.fn,
+          initialState: {
+            modal: {
+              modalList: {
+                showBooking: false,
+              }
+            }
+          }
+        })],
+      },
+    })
+
+    expect(wrapper.isVisible()).toBe(false)
+    expect(wrapper.html()).toBe('<!--v-if-->')
+  });
+  
+  test('text should be there if {showBooking: true }', () => {
+    wrapper = mount(BookingElement, {
+      global: {
+        plugins: [createTestingPinia({
+          // createSpy: vi.fn,
+          initialState: {
+            modal: {
+              modalList: {
+                showBooking: true,
+              }
+            }
+          }
+        })],
+      },
+    })
+
+    expect(wrapper.findAll('h3').at(0).text()).toContain('Book Your Table')
+    expect(wrapper.findAll('p').at(0).text()).toContain('Section under construction...')
+  });
 })
