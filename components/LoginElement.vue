1d071e664f363579a13ebd4c7660dcc314a65f82
arpado <ozsvar.arpad@gmail.com>
2023-03-31 12:53:54 +0200

    styles added

--- components/LoginElement.vue
+++ components/LoginElement.vue
@@ -3,27 +3,47 @@
     <NuxtLayout name="modal">
       <div class="login-container flex column justify-evenly">
         <h3>LOGIN</h3>
+        <hr class="title-hr" />
         <!-- <form action="POST"> -->
-        <form v-on:submit.prevent="userStore.login(email, password)" action="#" method="POST">
-          <label for="email">Email:</label>
-          <input type="email" name="email" id="email" v-model="email" @keyup.enter="userStore.login(email, password)"/>
-          <hr />
-          <label for="password">Password:</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            v-model="password"
-            @keyup.enter="userStore.login(email, password)"
-          />
-          <hr />
+        <form
+          v-on:submit.prevent="login(email, password)"
+          action="#"
+          method="POST"
+        >
+          <div class="input-box flex justify-center align-start column">
+            <label for="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              v-model="email"
+              @keyup.enter="login(email, password)"
+            />
+            <hr />
+          </div>
+          <div class="input-box flex justify-center align-start column">
+            <label for="password">Password:</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              v-model="password"
+              @keyup.enter="login(email, password)"
+            />
+            <hr />
+          </div>
         </form>
         <div class="error-container" v-if="hasError">
           <h3>Error!</h3>
           <p v-if="errorMessage">{{ errorMessage }}</p>
           <p v-else>Something went wrong!</p>
         </div>
-        <button type="submit" @click="userStore.login(email, password)">Login</button>
+        <ButtonElement
+          class="self-center"
+          text="Login"
+          type="submit"
+          @click="login(email, password)"
+        />
         <p>
           Not registered yet?
           <NuxtLink to="/user-registration" @click="modalStore.closeModal"
@@ -37,8 +57,8 @@
 
 <script>
 import { useModalStore } from "@/stores/modalStore";
-import { useUserStore } from "@/stores/userStore"
-import { postLoginForm } from "@/composables/userAuth"
+import { useUserStore } from "@/stores/userStore";
+import { postLoginForm } from "@/composables/userAuth";
 
 export default {
   setup() {
@@ -57,6 +77,16 @@ export default {
       errorMessage: null,
     };
   },
+  methods: {
+    async login(email, password) {
+      try {
+        let { data, error } = await this.userStore.login(email, password);
+        this.modalStore.closeModal();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 };
 </script>
 
@@ -69,6 +99,14 @@ export default {
 .login-container {
   min-height: 70vh;
   padding: 1rem;
+  max-width: 400px;
+}
+h3 {
+  font-size: 1.5rem;
+}
+.title-hr {
+  width: 50%;
+  margin: 5px auto 10px;
 }
 form > * {
   display: block;
