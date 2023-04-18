10dde7ba0a08c9547ac6d86f611cd77d681d2bf9
arpado <ozsvar.arpad@gmail.com>
2023-04-18 10:38:48 +0200

    the html and css for the user history done

--- components/UserHistoryElement.vue
+++ components/UserHistoryElement.vue
@@ -3,15 +3,38 @@
     <h3>User History</h3>
     <div class="flex column" v-if="userStore.orderHistory">
       <div
-        class="order-history"
-        v-for="(item, index) in userStore.orderHistory"
+        class="orders-container"
+        v-for="(order, index) in userStore.orderHistory"
         :key="index"
       >
-        <!-- {{ item }} -->
-        <p>
-          {{ item.item_id }} -- {{ item.item_option }} --
-          {{ item.item_quantity }}
-        </p>
+        <p class="order-id">Order ID: {{ order.orderId }}</p>
+        <div
+          class="item-container flex column justify-between align-start"
+          v-for="(item, index) in order.items"
+          :key="index"
+        >
+          <div class="name-container flex justify-between align-center">
+            <p class="item-name">Item Name: {{ item.name }}</p>
+            <div class="flex justify-center align-end column">
+              <p>Item Quantity: {{ item.quantity }}</p>
+              <p>Item Price: $0</p>
+            </div>
+          </div>
+          <div>Item Size: {{ item.size }}</div>
+          <div v-for="(prop, index) in item.props" :key="index">
+            <p v-for="(value, key, index) in prop" :key="index">
+              Item {{ key }}: {{ value }}
+            </p>
+          </div>
+          <div>
+            <span>Additional Options Selected: </span>
+            <span v-for="(option, index) in item.options" :key="index"
+              >{{ option }},
+            </span>
+          </div>
+        </div>
+        <p class="order-total">Order Total: ${{ order.orderTotal }}</p>
+        <hr />
       </div>
     </div>
     <ButtonElement text="Get History" @click="userStore.getUserOrderHistory" />
@@ -19,14 +42,8 @@
 </template>
 
 <script>
-// import { useUserStore } from "@/stores/userStore";
-
 export default {
-//   setup() {
-//     const userStore = useUserStore();
-//     return { userStore };
-//   },
-  props: ['userStore']
+  props: ["userStore"],
 };
 </script>
 
@@ -37,4 +54,20 @@ export default {
     min-width: 400px;
   max-width: 90vw;
 } */
+.orders-container {
+  padding: 1rem;
+  /* margin: 1rem; */
+}
+.item-container {
+  border: 1px solid black;
+  border-radius: 10px;
+  margin: 10px 5px;
+  padding: 1rem;
+}
+.name-container {
+  width: 100%;
+}
+.order-total {
+  text-align: right;
+}
 </style>
